4486798126c608af0b24d7e5ad5477551903539d
KBTREY35 <kevinbhangu12@gmail.com>
2025-03-28 19:29:16 -0400

    Initial Commit

---
+++ A2_IOS_Kevin_101418717/A2_IOS_Kevin_101418717App.swift
@@ -0,0 +1,20 @@
+//
+//  A2_IOS_Kevin_101418717App.swift
+//  A2_IOS_Kevin_101418717
+//
+//  Created by kevin bhangu on 2025-03-28.
+//
+
+import SwiftUI
+
+@main
+struct A2_IOS_Kevin_101418717App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
